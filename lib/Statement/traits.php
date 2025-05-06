e42adaa8df8315f476c5b232438339600bbcec17
Zatnosk <zatnosk@zatnosk.dk>
2025-05-06 09:57:58 +0200

    Corrected link to license file. Fixes #2

--- lib/Statement/traits.php
+++ lib/Statement/traits.php
@@ -1,7 +1,7 @@
 <?php
 /*
 BeaverQuery is licensed under the Apache License 2.0 license
-https://github.com/TRP-Solutions/dbtool/blob/master/LICENSE
+https://github.com/TRP-Solutions/beaver-query/blob/main/LICENSE.txt
 */
 declare(strict_types=1);
 namespace TRP\BeaverQuery\Statement;
